2572c3fadf16ecf50f5b128bc8702c661a4160c7
Shobhit Ranjan <shobhit.ranjan098@gmail.com>
2020-11-22 20:33:06 +0530

    Amit

--- coders/settings.py
+++ coders/settings.py
@@ -16,6 +16,8 @@ import django_heroku
 import dj_database_url
 from decouple import config
 
+#amit kumar
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
