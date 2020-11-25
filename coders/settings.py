287dbeeccb64cc0f6bd7fa17015b70d0eea265a1
Shobhit Ranjan <shobhit.ranjan098@gmail.com>
2020-11-25 19:33:05 +0530

    update

--- coders/settings.py
+++ coders/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '&j7zhb4&#g=st=t)q4cr^x#qt!*yb(ra3i)58#-yhy_51l-932'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
